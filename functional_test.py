34cf196ec72691edbe1bdf9082bf3b2505706f4d
Sayyid <hafiyyan94@gmail.com>
2017-01-26 13:57:05 +0700

    Chapter 5, Adding several elements for to-do lists

--- functional_test.py
+++ functional_test.py
@@ -41,17 +41,27 @@ class NewVisitorCome(unittest.TestCase):
 
         #When unexpected error come, use time.sleep as last measure
         #time.sleep(10)
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
-        
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
 
         # There is still a text box inviting her to add another item. She
         # enters "Use peacock feathers to make a fly" (Edith is very
         # methodical)
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Use peacock feathers to make a fly')
+        inputbox.send_keys(Keys.ENTER)
+
+        # The page updates again, and now shows both items on her list
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
+        self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
         self.fail('Finish the test!')
 
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
 
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        pass
 
 if __name__ == '__main__':
     unittest.main(warnings='ignore')
