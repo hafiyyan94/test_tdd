cff6026da73c20035059eda8a30859c092510d47
Sayyid <hafiyyan94@gmail.com>
2017-02-01 10:54:27 +0700

    Chapter 6, new URL + view for adding to existing lists. FT passes :-)

--- superlists/views.py
+++ superlists/views.py
@@ -19,5 +19,14 @@ def view_list(request):
 def new_list(request):
     list_ = List.objects.create()
     Item.objects.create(text=request.POST['item_text'], list=list_)
-    return redirect('/lists/the-only-list-in-the-world/')
+    return redirect('/lists/%d/' % (list_.id,))
 
+def view_list(request, list_id):
+    list_ = List.objects.get(id=list_id)
+    items = Item.objects.filter(list=list_)
+    return render(request, 'list.html', {'list': list_})
+
+def add_item(request, list_id):
+    list_ = List.objects.get(id=list_id)
+    Item.objects.create(text=request.POST['item_text'], list=list_)
+    return redirect('/lists/%d/' % (list_.id,))
