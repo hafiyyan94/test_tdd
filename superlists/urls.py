39b0473ba2dfe4381bb50399925573ff02a0b79e
Sayyid <hafiyyan94@gmail.com>
2017-02-01 11:24:14 +0700

    New branch

--- superlists/urls.py
+++ superlists/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url
 from superlists import views
 
+#url for superlists
 urlpatterns = [
     url(r'^new$', views.new_list, name='new_list'),
     url(r'^(\d+)/$', views.view_list, name='view_list'),
