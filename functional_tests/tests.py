c7edaa39e43e12dc2d703a56dfc65837a40a92e8
Sayyid <hafiyyan94@gmail.com>
2017-02-02 14:33:00 +0700

    Part 2, Chapter 8, Preparing functional test for liveserver test and deployment

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -3,10 +3,21 @@ from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
 from selenium.webdriver.common.keys import Keys
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 import unittest
+import sys
 import time
 
 #Testing web logic, and page redirection
 class NewVisitorCome(StaticLiveServerTestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        for arg in sys.argv:
+            if 'liveserver' in arg:
+                cls.server_url = 'http://' + arg.split('=')[1]
+                return
+        super().setUpClass()
+        cls.server_url = cls.live_server_url
+
     def setUp(self):
         self.browser = webdriver.Firefox(firefox_binary=FirefoxBinary(
             firefox_path='D:\\PyCharm_Project\\Mozilla_ESR\\firefox.exe'
@@ -21,7 +32,7 @@ class NewVisitorCome(StaticLiveServerTestCase):
         # Edith has heard about a cool new online to-do app. She goes
         # to check out its homepage
         #self.browser.get('http://localhost:8000')
-        self.browser.get(self.live_server_url)
+        self.browser.get(self.server_url)
         # She notices the page title and header mention to-do lists
         self.assertIn('To-Do', self.browser.title)
         header_text = self.browser.find_element_by_tag_name('h1').text
@@ -34,7 +45,7 @@ class NewVisitorCome(StaticLiveServerTestCase):
 
     def test_can_start_a_list_and_retrieve_it_later(self):
         #self.browser.get('http://localhost:8000')
-        self.browser.get(self.live_server_url)
+        self.browser.get(self.server_url)
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Buy peacock feathers')
         inputbox.send_keys(Keys.ENTER)
@@ -56,7 +67,7 @@ class NewVisitorCome(StaticLiveServerTestCase):
         # Edith has heard about a cool new online to-do app. She goes
         # to check out its homepage
         #self.browser.get('http://localhost:8000')
-        self.browser.get(self.live_server_url)
+        self.browser.get(self.server_url)
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Buy peacock feathers')
         inputbox.send_keys(Keys.ENTER)
@@ -78,7 +89,7 @@ class NewVisitorCome(StaticLiveServerTestCase):
         # Francis visits the home page.  There is no sign of Edith's
         # list
         #self.browser.get('http://localhost:8000')
-        self.browser.get(self.live_server_url)
+        self.browser.get(self.server_url)
         page_text = self.browser.find_element_by_tag_name('body').text
         self.assertNotIn('Buy peacock feathers', page_text)
         self.assertNotIn('make a fly', page_text)
@@ -104,6 +115,17 @@ class NewVisitorCome(StaticLiveServerTestCase):
 
 #Testing Layouot and UI
 class NewVisitorTest(StaticLiveServerTestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        for arg in sys.argv:
+            if 'liveserver' in arg:
+                cls.server_url = 'http://' + arg.split('=')[1]
+                return
+        super().setUpClass()
+        cls.server_url = cls.live_server_url
+
+
     def setUp(self):
         self.browser = webdriver.Firefox(firefox_binary=FirefoxBinary(
             firefox_path='D:\\PyCharm_Project\\Mozilla_ESR\\firefox.exe'
@@ -117,7 +139,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
     #Test layout home page
     def test_layout_and_styling(self):
         # Edith goes to the home page
-        self.browser.get(self.live_server_url)
+        self.browser.get(self.server_url)
         self.browser.set_window_size(1024, 768)
 
         # She notices the input box is nicely centered
