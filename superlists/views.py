ecf437057266ef282b6fce82b8f5f38412e55028
Sayyid <hafiyyan94@gmail.com>
2017-02-03 16:09:09 +0700

    Part 2, Chapter 8, Refactor list view to handle new item POSTs

--- superlists/views.py
+++ superlists/views.py
@@ -11,11 +11,6 @@ def home_page(request):
     return render(request, 'home.html')
 
 
-#Page that show the list
-def view_list(request):
-    items = Item.objects.all()
-    return render(request, 'list.html', {'items': items})
-
 #Page for submitting new to do lists
 def new_list(request):
     list_ = List.objects.create()
@@ -31,10 +26,7 @@ def new_list(request):
 
 def view_list(request, list_id):
     list_ = List.objects.get(id=list_id)
-    items = Item.objects.filter(list=list_)
+    if request.method == 'POST':
+        Item.objects.create(text=request.POST['item_text'], list=list_)
+        return redirect('/lists/%d/' % (list_.id,))
     return render(request, 'list.html', {'list': list_})
-
-def add_item(request, list_id):
-    list_ = List.objects.get(id=list_id)
-    Item.objects.create(text=request.POST['item_text'], list=list_)
-    return redirect('/lists/%d/' % (list_.id,))
